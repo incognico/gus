77cd620ac6277b2d1c404f8baa54ebae703b9046
incognico <nico@lifeisabug.com>
2018-06-24 01:33:58 +0200

    add !xonstat

--- gus.pl
+++ gus.pl
@@ -36,9 +36,9 @@ my $self;
 my $lastmap = '';
 
 my $config = {
-   chatlinkchan => "390586588897345540",
-   fancystatuschan => "368487578028081154",
-   adminrole => "368491069874241536",
+   chatlinkchan => "458683388887302155",
+   fancystatuschan => "458323696910598167",
+   adminrole => "",
    fromsven => "$ENV{HOME}/sc5/svencoop/scripts/plugins/store/_fromsven.txt",
    tosven => "$ENV{HOME}/sc5/svencoop/scripts/plugins/store/_tosven.txt",
    db => "$ENV{HOME}/scstats/scstats.db",
@@ -47,13 +47,14 @@ my $config = {
    steamapiurl2 => "http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=XXXSTEAMAPIKEYXXX&steamids=",
    serverport => "27210",
    gmapikey => "",
+   xonstaturl => "http://stats.xonotic.org/player/",
 
    discord => {
      auto_reconnect => 1,
-     client_id => "",
+     client_id => "393059875871260672",
      name => "Gus",
      owner_id => "373912992758235148",
-     game => "Sven Co-op @ twlz",
+#     game => "Sven Co-op @ twlz",
      token => "",
      verbose => 0,
    }
@@ -75,24 +76,27 @@ my $discord = Mojo::Discord->new(
 );
 
 my $maps = {
-   'hl_c01_a1' => '<:hl:369091257999294464> Half-Life',
-   'of1a1' => '<:of:370226020982325250> Opposing Force',
-   'ba_security1' => '<:bs:370225849426771979> Blue Shift',
-   'escape_series_1a' => '<:sc:370225689514737665> Escape Series: Part 1',
-   'escape_series_2a' => '<:sc:370225689514737665> Escape Series: Part 2',
-   'escape_series_3a' => '<:sc:370225689514737665> Escape Series: Part 3',
-   'etc' => '<:sc:370225689514737665> Earthquake Test Center',
-   'etc2_1' => '<:sc:370225689514737665> Earthquake Test Center 2',
-   'mistake_coop_a' => '<:sc:370225689514737665> Mistake Co-op',
-   'po_c1m1' => '<:sc:370225689514737665> Poke 646',
-   'po_c1m1' => '<:sc:370225689514737665> Poke 646: Vendetta',
-   'rl02' => '<:sc:370225689514737665> Residual Life',
-   'th_ep1_00' => '<:th:372377213779312640> They Hunger: Episode 1',
-   'th_ep2_00' => '<:th:372377213779312640> They Hunger: Episode 2',
-   'th_ep3_00' => '<:th:372377213779312640> They Hunger: Episode 3',
-   'th_escape' => '<:th:372377213779312640> Woohoo, They Hunger: Escape',
-   'road_to_shinnen' => '<:twlz:370619463038664705> Oh god, oh no, Road to Shinnen',
-   'sc_tl_build_puzzle_fft_final' => '<:lul:370224421933285386> Build Puzzle',
+   'hl_c01_a1' => '<:sven:459617478365020203> Half-Life',
+   'of1a1' => '<:sven:459617478365020203> Opposing Force',
+   'ba_security1' => '<:sven:459617478365020203> Blue Shift',
+   'escape_series_1a' => '<:sven:459617478365020203> Escape Series: Part 1',
+   'escape_series_2a' => '<:sven:459617478365020203> Escape Series: Part 2',
+   'escape_series_3a' => '<:sven:459617478365020203> Escape Series: Part 3',
+   'etc' => '<:sven:459617478365020203> Earthquake Test Center',
+   'etc2_1' => '<:sven:459617478365020203> Earthquake Test Center 2',
+   'mistake_coop_a' => '<:sven:459617478365020203> Mistake Co-op',
+   'po_c1m1' => '<:sven:459617478365020203> Poke 646',
+   'po_c1m1' => '<:sven:459617478365020203> Poke 646: Vendetta',
+   'rl02' => '<:sven:459617478365020203> Residual Life',
+   'th_ep1_00' => '<:sven:459617478365020203> They Hunger: Episode 1',
+   'th_ep2_00' => '<:sven:459617478365020203> They Hunger: Episode 2',
+   'th_ep3_00' => '<:sven:459617478365020203> They Hunger: Episode 3',
+   'th_escape' => '<:flower:458608402549964814> Woohoo, They Hunger: Escape',
+   'road_to_shinnen' => '<:owo:459363994403209216> Oh god, oh no, Road to Shinnen',
+   'rust_island_b5' => '<:pog:458682189471809536> R U S T',
+   'rust_island_b6' => '<:pog:458682189471809536> R U S T',
+   'sc_tl_build_puzzle_fft_final' => '<:lul:458685800725610516> Build Puzzle',
+   'shl0' => ':candy: Sweet Half-Life', 
 };
 
 my @winddesc = (
@@ -185,7 +189,9 @@ my $filestream = IO::Async::FileStream->new(
 
             $line =~ s/`/\`/g;
             $line =~ s/^<(.+)><STEAM_0.+> (.+)/`$1`  $2/g;
-            $line =~ s/\@ADMINS?/<@&$$config{'adminrole'}>/gi;
+            #$line =~ s/\@ADMINS?/<@&$$config{'adminrole'}>/gi;
+            $line =~ s/\@everyone/everyone/g;
+            $line =~ s/\@here/here/g;
 
             $discord->send_message( $$config{'chatlinkchan'}, $line );
          }
@@ -256,12 +262,12 @@ sub discord_on_message_create
 
          if ( $param =~ /^STEAM_(0:[01]:[0-9]+)$/ )
          {
-            $stmt = "SELECT * FROM stats WHERE steamid = ? ORDER BY score DESC LIMIT 1";
+            $stmt = "SELECT * FROM stats WHERE steamid = ? ORDER BY date(seen) DESC, score DESC LIMIT 1";
             @bind = ( "$1" );
          }
          else
          {
-            $stmt = "SELECT * FROM stats WHERE name LIKE ? ORDER BY score DESC LIMIT 1";
+            $stmt = "SELECT * FROM stats WHERE name LIKE ? ORDER BY date(seen) DESC, score DESC LIMIT 1";
             @bind = ( "%$1%" );
          }
 
@@ -312,11 +318,8 @@ sub discord_on_message_create
                 'thumbnail' => {
                    'url' => $$result{'response'}{'players'}->[0]{avatarfull},
                 },
-                'footer' => {
-                   'text' => "Approximate location: $loc",
-                },
 #                'image' => {
-#                   'url' => "https://maps.googleapis.com/maps/api/staticmap?size=360x80&scale=2&language=en&region=ps&center=$r->[12],$r->[13]&zoom=7&key=$$config{'gmapikey'}",
+#                   'url' => "https://maps.googleapis.com/maps/api/staticmap?size=360x80&scale=2&language=en&region=ps&center=$r->[12],$r->[13]&zoom=7",
 #                   'width' => 360,
 #                   'height' => 80,
 #                },
@@ -482,7 +485,7 @@ sub discord_on_message_create
                 'height' => 38,
              },
              'image' => {
-                'url' => "https://maps.googleapis.com/maps/api/staticmap?size=360x80&scale=2&language=en&region=ps&center=$lat,$lon&zoom=8&key=$$config{'gmapikey'}",
+                'url' => "https://maps.googleapis.com/maps/api/staticmap?size=360x80&scale=2&language=en&region=ps&center=$lat,$lon&zoom=8",
                 'width' => 360,
                 'height' => 80,
              },
@@ -491,7 +494,7 @@ sub discord_on_message_create
              },
              'fields' => [
              {
-                'name'   => $flag eq 'flag_se' ? ':gay_pride_flag:' : ":$flag:" . ' Weather for:',
+                'name'   => ( $flag eq 'flag_se' ? ':gay_pride_flag:' : ":$flag:" ) . ' Weather for:',
                 'value'  => "**[$loc](https://www.google.com/maps/\@$lat,$lon,13z)**",
                 'inline' => \0,
               },
@@ -567,6 +570,95 @@ sub discord_on_message_create
          {
             $discord->send_message( $channel, '`API error`' );
          }
+      }
+      elsif ( $channel ne $$config{'chatlinkchan'} && $msg =~ /^!xonstat (.+)/i ) {
+         my ($qid, $stats);
+         ($qid = $1) =~ s/[^0-9]//g;
+
+         unless ($qid) {
+            $discord->send_message( $channel, 'invalid player id');
+            return;
+         }
+
+         my $json = get($$config{'xonstaturl'} . $qid . '.json');
+
+         if ($json) {
+            eval { $stats = decode_json($json) };
+         }
+         else {
+            $discord->send_message( $channel, 'no response from server; correct player id?');
+            return;
+         }
+
+         my $snick   = $stats->[0]->{player}->{stripped_nick};
+         my $games   = $stats->[0]->{games_played}->{overall}->{games};
+         my $win     = $stats->[0]->{games_played}->{overall}->{wins};
+         my $loss    = $stats->[0]->{games_played}->{overall}->{losses};
+         my $pct     = $stats->[0]->{games_played}->{overall}->{win_pct};
+         my $kills   = $stats->[0]->{overall_stats}->{overall}->{total_kills};
+         my $deaths  = $stats->[0]->{overall_stats}->{overall}->{total_deaths};
+         my $ratio   = $stats->[0]->{overall_stats}->{overall}->{k_d_ratio};
+         my $elo     = $stats->[0]->{elos}->{overall}->{elo} ? $stats->[0]->{elos}->{overall}->{elo}          : 0;
+         my $elot    = $stats->[0]->{elos}->{overall}->{elo} ? $stats->[0]->{elos}->{overall}->{game_type_cd} : 0;
+         my $elog    = $stats->[0]->{elos}->{overall}->{elo} ? $stats->[0]->{elos}->{overall}->{games}        : 0;
+         my $capr    = $stats->[0]->{overall_stats}->{ctf}->{cap_ratio} ? $stats->[0]->{overall_stats}->{ctf}->{cap_ratio} : 0;
+         my $favmap  = $stats->[0]->{fav_maps}->{overall}->{map_name};
+         my $favmapt = $stats->[0]->{fav_maps}->{overall}->{game_type_cd};
+         my $last    = $stats->[0]->{overall_stats}->{overall}->{last_played_fuzzy};
+
+         my $embed = {
+            'color' => '15844367',
+            'provider' => {
+               'name' => 'XonStat',
+               'url' => 'http://stats.xonotic.org',
+             },
+             #'thumbnail' => {
+             #   'url' => "https://cdn.discordapp.com/emojis/458355320364859393.png?v=1",
+             #   'width' => 38,
+             #   'height' => 38,
+             #},
+             'image' => {
+                'url' => "http://stats.xonotic.org/static/badges/$qid.png",
+                'width' => 650,
+                'height' => 70,
+             },
+             'footer' => {
+                'text' => "Last played: $last",
+             },
+             'fields' => [
+              {
+                 'name'   => 'Name',
+                 'value'  => "**[$snick]($$config{'xonstaturl'}$qid)**",
+                 'inline' => \1,
+              },
+              {
+                 'name'   => 'Games Played',
+                 'value'  => $games,
+                 'inline' => \1,
+              },
+              {
+                 'name'   => 'Favourite Map',
+                 'value'  => sprintf('%s (%s)', $favmap, $favmapt),
+                 'inline' => \1,
+              },
+              {
+                 'name'   => 'Cap Ratio',
+                 'value'  => $capr ? sprintf('%.2f', $capr) : '-',
+                 'inline' => \1,
+              },
+              ],
+         };
+
+         my $message = {
+            'content' => '',
+            'embed' => $embed,
+         };
+
+         $discord->send_message( $channel, $message );
+      }
+      elsif ( $channel ne $$config{'chatlinkchan'} && $msg =~ /^!xon$/i )
+      {
+         $discord->send_message( $channel, '<:xon:458355320364859393> IPv4: `connect 148.251.69.59` IPv6: `connect 2a01:4f8:202:3328::2`' );
       }
    }
 }
