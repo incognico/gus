7a7fd8a5e00db7c49308331c144575c5c5262ccf
incognico <nico@lifeisabug.com>
2021-03-28 13:14:33 +0200

    gus

--- gus.pl
+++ gus.pl
@@ -991,6 +991,13 @@ sub discord_on_message_create
          # TODO: random time 3h-3d when "remind me to ..."?
          {
             my $target = ( !defined $+{target} || fc($+{target}) eq fc('me') ) ? "<\@$id>" : $+{target};
+
+            unless ($target =~ /<@!?(\d+)>/)
+            {
+               react( $channel, $msgid, 'what' );
+               return;
+            }
+
             my $delay  = $+{mins};
             my $text   = defined $+{text} ? $+{text} : "\N{U+23F0}";
 
